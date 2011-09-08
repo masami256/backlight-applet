5fc8dca3a3192293b08aa7b1dc17881e565af622
Masami Ichikawa <masami256@gmail.com>
2011-09-08 23:29:13 +0900

    Added new functions.

--- src/backlight.c
+++ src/backlight.c
@@ -39,9 +39,125 @@ typedef struct {
      Display *display;
      Atom backlight_new;
      Atom backlight_legacy;
-     int brightness;
+     Atom backlight_cur;
+     double brightness;
+     double next_value;
 } backlight_t;
 
+enum {
+     GetBrightness = 0,
+     SetBrightness,
+};
+
+static long backlight_get(backlight_t *bklight, RROutput output)
+{
+     unsigned long nitems;
+     unsigned long bytes_after;
+     unsigned char *prop;
+     Atom actual_type;
+     int actual_format;
+     long value;
+    
+     bklight->backlight_cur = bklight->backlight_new;
+     if (!bklight->backlight_cur ||
+	 XRRGetOutputProperty(bklight->display, output, bklight->backlight_cur,
+			       0, 4, False, False, None,
+			       &actual_type, &actual_format,
+			       &nitems, &bytes_after, &prop) != Success) {
+	  bklight->backlight_cur = bklight->backlight_legacy;
+	  if (!bklight->backlight_cur ||
+	      XRRGetOutputProperty(bklight->display, output, bklight->backlight_cur,
+				    0, 4, False, False, None,
+				    &actual_type, &actual_format,
+				    &nitems, &bytes_after, &prop) != Success)
+	       return -1;
+     }
+
+     if (actual_type != XA_INTEGER || nitems != 1 || actual_format != 32)
+	  value = -1;
+     else
+	  value = *((long *) prop);
+     XFree(prop);
+     return value;
+}
+
+static void backlight_set(backlight_t *bklight, RROutput output, long value)
+{
+     XRRChangeOutputProperty(bklight->display, output, bklight->backlight_cur, XA_INTEGER, 32,
+			     PropModeReplace, (unsigned char *) &value, 1);
+}
+
+static void property_operation(backlight_t *bklight, double cur, RROutput output, int operation)
+{
+     XRRPropertyInfo *info;
+     double min, max;
+
+     if ((info = XRRQueryOutputProperty(bklight->display, output, bklight->backlight_cur)) != NULL) {
+	  if (info->range && info->num_values == 2) {
+	       min = info->values[0];
+	       max = info->values[1];
+	       if (operation == GetBrightness) {
+		    bklight->brightness =  (cur - min) * 100 / (max - min);
+	       } else {
+		    double tmp = bklight->next_value * (max - min) / 100;
+		    double new = min + tmp;
+		    double step = 0.0;
+		    int i = 0;
+
+		    if (new > max) new = max;
+		    if (new < min) new = min;
+		    step = (new - cur) / 20;
+
+		    for (i = 0; i < 20; i++) {
+			 if (i == 19)
+			      cur = new;
+			 else
+			      cur += step;
+			 backlight_set (bklight, output, (long) cur);
+			 XFlush(bklight->display);
+			 usleep (200);
+		    }
+		    bklight->brightness = new;
+	       }
+	  }
+	  XFree(info);	  
+     }
+		 		      
+}
+
+static void walk_each_screen(backlight_t *bklight, XRRScreenResources *resources, int operation)
+{
+     int i = 0;
+
+     for (i = 0; i < resources->noutput; i++) {
+	    RROutput output = resources->outputs[i];
+	    double cur = 0.0;
+
+	    cur = backlight_get(bklight, output);
+
+	    if (cur != -1) {
+		 property_operation(bklight, cur, output, operation);
+	    }
+     }
+}
+
+static void screen_walker(backlight_t *bklight, int operation)
+{
+     int i = 0;
+     int count = ScreenCount(bklight->display);
+
+     for (i = 0; i < count; i++) {
+	  Window root = RootWindow(bklight->display, i);
+	  XRRScreenResources *resources = XRRGetScreenResources(bklight->display, root);
+
+	  if (resources) {
+	       walk_each_screen(bklight, resources, operation);
+	       XRRFreeScreenResources(resources);
+	    
+	  }
+     }
+     XSync(bklight->display, False);
+}
 
 static gboolean get_display(backlight_t *bklight)
 {
@@ -97,11 +213,24 @@ static gboolean open_display_and_check(backlight_t *bklight)
 
 }
 
+static void get_brightness_value(backlight_t *bklight)
+{
+     screen_walker(bklight, GetBrightness);
+}
+
 static gboolean focus_out_event(GtkWidget *widget, GdkEvent *event, backlight_t *bklight)
 {
      gtk_widget_hide(bklight->dlg);
      bklight->show = 0;
      return FALSE;
+}
+
+static void set_tooltip_text(backlight_t *bklight)
+{
+     char s[64] = { 0 };
+     g_snprintf(s, sizeof(s), _("Brightness %.1f%%"), bklight->brightness);
+
+     gtk_widget_set_tooltip_text(bklight->mainw, s);
 }
 
 static gboolean tray_icon_press(GtkWidget *widget, GdkEventButton *event, backlight_t *bklight)
@@ -122,32 +251,30 @@ static gboolean tray_icon_press(GtkWidget *widget, GdkEventButton *event, backli
 	  gtk_widget_hide(bklight->dlg);
 	  bklight->show = 0;
      }
+     set_tooltip_text(bklight);
      return TRUE;
 }
 
 static void on_vscale_value_changed(GtkRange *range, backlight_t *bklight)
 {
      // brightness_change
+     screen_walker(bklight, SetBrightness);
 }
 
 static void on_vscale_scrolled( GtkScale* scale, GdkEventScroll *evt, backlight_t* bklight )
 {
-     gdouble val = gtk_range_get_value((GtkRange*) scale);
-     switch(evt->direction) {
+     bklight->next_value = gtk_range_get_value((GtkRange*) scale);
+     switch( evt->direction ) {
      case GDK_SCROLL_UP:
-	  val += 1.0;
+	  bklight->next_value += 1;
 	  break;
      case GDK_SCROLL_DOWN:
-	  val -= 1.0;
+	  bklight->next_value -= 1;
 	  break;
      }
-     gtk_range_set_value((GtkRange*) scale, CLAMP((int) val, 0, 100));
+     gtk_range_set_value((GtkRange*)scale, CLAMP((int) bklight->next_value, 0, 100) );
 }
 
-static int backlight_value_get(void)
-{
-     return 100;
-}
 
 static void panel_init(Plugin *p)
 {
@@ -170,7 +297,7 @@ static void panel_init(Plugin *p)
      gtk_window_set_type_hint(GTK_WINDOW(bklight->dlg), GDK_WINDOW_TYPE_HINT_DIALOG);
 
      /* Focus-out signal */
-     g_signal_connect (G_OBJECT (bklight->dlg), "focus_out_event",
+      g_signal_connect (G_OBJECT (bklight->dlg), "focus_out_event",
 		       G_CALLBACK (focus_out_event), bklight);
 
      scrolledwindow = gtk_scrolled_window_new(NULL, NULL);
@@ -187,7 +314,7 @@ static void panel_init(Plugin *p)
      gtk_widget_show(viewport);
 
      /* create frame */
-     frame = gtk_frame_new(_("backlight"));
+     frame = gtk_frame_new(_("Brightness"));
      gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
      gtk_container_add(GTK_CONTAINER(viewport), frame);
 
@@ -195,7 +322,7 @@ static void panel_init(Plugin *p)
      box = gtk_vbox_new(FALSE, 0);
 
      /* create controller */
-     bklight->vscale = gtk_vscale_new(GTK_ADJUSTMENT(gtk_adjustment_new(backlight_value_get(), 0, 100, 0, 0, 0)));
+     bklight->vscale = gtk_vscale_new(GTK_ADJUSTMENT(gtk_adjustment_new(0, 0, 100, 0, 0, 0)));
      gtk_scale_set_draw_value(GTK_SCALE(bklight->vscale), FALSE);
      gtk_range_set_inverted(GTK_RANGE(bklight->vscale), TRUE);
 
@@ -211,8 +338,7 @@ static void panel_init(Plugin *p)
      gtk_widget_set_style(viewport, p->panel->defstyle);
 }
 
-static void
-backlight_destructor(Plugin *p)
+static void backlight_destructor(Plugin *p)
 {
      backlight_t *bklight = (backlight_t *) p->priv;
 
@@ -230,8 +356,7 @@ backlight_destructor(Plugin *p)
      RET();
 }
 
-static int
-backlight_constructor(Plugin *p, char **fp)
+static int backlight_constructor(Plugin *p, char **fp)
 {
      backlight_t *bklight;
      GdkPixbuf *icon;
@@ -265,8 +390,12 @@ backlight_constructor(Plugin *p, char **fp)
 
      gtk_widget_show_all(bklight->mainw);
 
-     /* FIXME: display current level in tooltip. ex: "Brightness: 80%"  */
-     gtk_widget_set_tooltip_text(bklight->mainw, _("Brightness"));
+     bklight->brightness = -1;
+     if (open_display_and_check(bklight)) {
+	  get_brightness_value(bklight);
+
+	  set_tooltip_text(bklight);
+     }
 
      /* store the created plugin widget in plugin->pwid */
      p->pwid = bklight->mainw;
